fc4422a7e280f417df2957055503a8f5f7b7fb64
silvan-wxy <799118590@qq.com>
2026-03-05 17:01:24 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,11 @@ public class Main{
         
         // --- TODO
 
-        /* Repeat the above code to create another object  
-         * Pass different parameters to the MusicBox constructor, but initialise premiumSong to N and when calling the
+MusicBox mbox2 = new MusicBox("A12BC34", 'N', "Sunshine", "Bright", 
+    "Happy day", "Pop", "Star Music", "Sunny Vibes");
+
+
+playsong.playSong(mbox2.getSongID(), mbox2.getPremiumSong(), 1); to the MusicBox constructor, but initialise premiumSong to N and when calling the
          * playSong method, pass ads parameter as just 1
          * 
          * 
