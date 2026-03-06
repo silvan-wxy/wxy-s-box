397a49a9ade29f9ded554ff29b2195edf349f738
silvan-wxy <799118590@qq.com>
2026-03-06 11:35:53 +0800

    Update PlaySongs.java

--- src/PlaySongs.java
+++ src/PlaySongs.java
@@ -1,26 +1,29 @@
 public class PlaySongs {
     public void playSong(String songID, char premium, int ads) {
-        switch(premium){
+        switch (premium) {
             case 'Y':
-            System.out.println("This is a premium song. Please buy it to play without the ads");
-            playAd(ads, premium);
-            break;
+            case 'y':
+                System.out.println("This is a premium song. Please buy it to play without the ads.");
+                break;
             case 'N':
-            System.out.println("Thank you for choosing this song. Hope you enjoy listening to it.");
-            playAd(ads, premium);
-            break;
+            case 'n':
+                System.out.println("Thank you for choosing this song. Hope you enjoy listening to it...");
+                playAd(ads, premium);
+                break;
+            default:
+                System.out.println("Invalid premium status.");
         }
     }
 
     private void playAd(int ads, char premium) {
-        if (ads == 1) {
-            System.out.println("Playing Ad 1");
-        }
-        else {
-            for(int i=1; i<=ads; i++){
+        if (premium == 'N' || premium == 'n') {
+            if (ads <= 0) {
+                System.out.println("No ads to play.");
+                return;
+            }
+            for (int i = 1; i <= ads; i++) {
                 System.out.println("Playing Ad " + i);
             }
         }
-        
     }
 }
