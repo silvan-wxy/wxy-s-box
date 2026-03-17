774e0a26179784d1c9be9afae6b95a2e5784a478
silvan-wxy <799118590@qq.com>
2026-03-17 14:41:52 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,16 @@
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        
-        MusicBox mbox = new MusicBox("S34TG65", 'Y', "Raindrops", "Misty", 
-        "The path less traveled", "Country", "BZEE Music", "Rhythm Divine");
+        MusicBox mbox = new MusicBox("S34TG65", "Y", "Raindrops", "Misty",
+                "The path less traveled", "Country", "BZEE Music", "Rhythm Divine");
+
+        MusicBox mbox2 = new MusicBox("1234", "N", "ra", "light",
+                "bad day", "kpop", "sun Music", "Vibes");
 
         PlaySongs playsong = new PlaySongs();
-        
         playsong.playSong(mbox.getSongID(), mbox.getPremiumSong(), 2);
-
-        
-        // --- TODO
-
-MusicBox mbox2 = new MusicBox("1234", 'N', "ra", "light", 
-    "bad day", "kpop", "sun Music", "Vibes");
-
-
-playsong.playSong(mbox2.getSongID(), mbox2.getPremiumSong(), 1); to the MusicBox constructor, but initialise premiumSong to N and when calling the
-         * playSong method, pass ads parameter as just 1
-         * 
-         * 
-        */
-        
-
+        playsong.playSong(mbox2.getSongID(), mbox2.getPremiumSong(), 1);
+        UserRegistration userReg = new UserRegistration();
+        userReg.registration();
+        System.out.println(userReg);
     }
 }
-
-
-
-
