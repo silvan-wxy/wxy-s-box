a1cd8ec91eddb801c503de938957b6d3dfa99faf
silvan-wxy <799118590@qq.com>
2026-04-03 13:57:03 +0800

    Create BikeRequest.java

---
+++ src/BikeRequest.java
@@ -0,0 +1,30 @@
+import java.time.LocalDateTime;
+
+public class BikeRequest {
+    private String userEmail;
+    private String location;
+    private LocalDateTime requestTime;
+
+    public BikeRequest(String userEmail, String location, LocalDateTime requestTime) {
+        this.userEmail = userEmail;
+        this.location = location;
+        this.requestTime = requestTime;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public LocalDateTime getRequestTime() {
+        return requestTime;
+    }
+
+    @Override
+    public String toString() {
+        return userEmail + " - " + location + " - " + requestTime;
+    }
+}
