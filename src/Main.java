bd0158f574d11e2232a04f561547d5fdba570094
silvan-wxy <799118590@qq.com>
2026-04-03 13:55:11 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-
 public class Main {
     public static void main(String[] args) {
-        UserRegistration userReg = new UserRegistration();
-        userReg.registration();
-        System.out.println(userReg);
+        BikeDatabase bikeDatabase = new BikeDatabase();
+        BikeService bikeService = new BikeService(bikeDatabase);
+        AdminPanel adminPanel = new AdminPanel(bikeService);
+        adminPanel.showAdminMenu();
     }
 }
